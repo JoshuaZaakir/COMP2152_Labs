e864106fccd00fb235b83fe12963e3cf1e287c25
JoshuaZaakir <Joshua.Zaakir@georgebrown.ca>
2026-01-30 11:54:44 -0500

    add w03

---
+++ Week03/Q4.py
@@ -0,0 +1,6 @@
+monday_class = {"Alice", "Bob", "Charlie", "Diana"}
+wednesday_class = {"Bob", "Diana", "Eve", "Frank"}
+monday_class.add("Grace")
+print(f"Monday class: {monday_class}")
+print(f"Wednesday class: {wednesday_class}")
+print(f"Attended both classes {monday_class & wednesday_class}") # shift 7
