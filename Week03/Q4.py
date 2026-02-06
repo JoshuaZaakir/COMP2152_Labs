36ae590bd2bfef6ce4702ce11c0f1cae21707547
JoshuaZaakir <Joshua.Zaakir@georgebrown.ca>
2026-02-06 11:48:50 -0500

    add wk04

--- Week03/Q4.py
+++ Week03/Q4.py
@@ -4,3 +4,8 @@ monday_class.add("Grace")
 print(f"Monday class: {monday_class}")
 print(f"Wednesday class: {wednesday_class}")
 print(f"Attended both classes {monday_class & wednesday_class}") # shift 7
+print(f"Attended either classes {monday_class | wednesday_class}") # shift \
+print(f"Only Monday {monday_class - wednesday_class}")
+print(f"Only One class {monday_class ^ wednesday_class}") # ^ = Caret, shift 6
+allStudents = monday_class | wednesday_class
+print("Is Monday subset of all students? ", monday_class <= allStudents) # True
